da03dfa0f35dccaf30d6977a04e6c46b6bc83d90
Sima-Akther <saimaislambdk@gmail.com>
2026-03-07 16:14:21 +0600

    login succces

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+ function startLogin() {
+            const u = document.getElementById('user-input').value;
+            const p = document.getElementById('pass-input').value;
+
+            if (u === "admin" && p === "admin123") {
+                document.getElementById('login-area').style.display = 'none';
+                document.getElementById('main-app').classList.remove('hidden');
+                getIssues();
+            } else {
+                alert("password vul dile kemne khulbe vai");
+            }
+        }
